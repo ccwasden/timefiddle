ea93b7f8f0ab170ec4b705b9110b4a1eb1e5007c
Sean Hill <sean.daryl.hill@gmail.com>
2013-02-13 20:17:45 -0700

    Added some layouts

---
+++ public/views/head-tag.js
@@ -0,0 +1,6 @@
+document.write(	'<script type="text/javascript" src="../javascripts/jquery.min.js">'+
+				'</script><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+				'<link rel="stylesheet" type="text/css" href="../stylesheets/style.css" />'+
+				'<script type="text/javascript" src="../plugins/bootstrap/js/bootstrap.min.js">'+
+				'</script><link rel="stylesheet" type="text/css" href="../plugins/bootstrap/css/bootstrap-responsive.min.css">'+
+				'<link rel="stylesheet" type="text/css" href="../plugins/bootstrap/css/bootstrap.css">');
